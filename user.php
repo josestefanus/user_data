ab8b68ceb5806f3aa10a7260eaafaf8c9878d5de
josestefanus <83685512+josestefanus@users.noreply.github.com>
2021-10-02 14:34:54 +0700

    Add files via upload

---
+++ user.php
@@ -0,0 +1,1 @@
+[{ "id": "160419001", "student_name":"Vincent", "birth_of_date": "2000/01/01", "phone": "085363010101", "photo_url": "https://randomuser.me/api/portraits/men/1.jpg" },{ "id": "160419034", "student_name":"Nathan", "birth_of_date": "2000/02/02", "phone": "085363020202", "photo_url": "https://randomuser.me/api/portraits/men/2.jpg" }]
