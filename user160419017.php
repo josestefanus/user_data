f9ba01d29621e8f514015f7aeb1ef47177b4eb85
josestefanus <83685512+josestefanus@users.noreply.github.com>
2021-10-05 14:25:08 +0700

    Update user160419017.php

--- user160419017.php
+++ user160419017.php
@@ -1,1 +1,1 @@
-{ "id": "160419017", "student_name":"Nigel", "birth_of_date": "2000/04/04", "phone": "085363040404", "photo_url": "https://randomuser.me/api/portraits/men/4.jpg", "status": "Vaccinated" }
+{ "id": "160419017", "student_name":"Nigel", "birth_of_date": "2000/04/04", "phone": "085363040404", "photo_url": "https://randomuser.me/api/portraits/men/4.jpg", "status": "VACCINATED" }
