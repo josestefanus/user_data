da16b096c49b02d9954ba6b9d42542d476ee3471
josestefanus <83685512+josestefanus@users.noreply.github.com>
2021-10-05 14:11:44 +0700

    Update user160419009.php

--- user160419009.php
+++ user160419009.php
@@ -1,1 +1,1 @@
-{ "id": "160419009", "student_name":"Albertus", "birth_of_date": "2000/03/03", "phone": "085363030303", "photo_url": "https://randomuser.me/api/portraits/men/3.jpg" }
+{ "id": "160419009", "student_name":"Albertus", "birth_of_date": "2000/03/03", "phone": "085363030303", "photo_url": "https://randomuser.me/api/portraits/men/3.jpg", "status": "Vaccinated" }
