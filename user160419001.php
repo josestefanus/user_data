18eb1a9ccbe946c71aa894e22920efcd68d1b8d3
josestefanus <83685512+josestefanus@users.noreply.github.com>
2021-10-08 00:43:55 +0700

    Update user160419001.php

--- user160419001.php
+++ user160419001.php
@@ -1,1 +1,1 @@
-{ "id": "160419001", "user_name":"Vincent", "birth_of_date": "2000/01/01", "phone": "085363010101", "photo_url": "https://randomuser.me/api/portraits/men/1.jpg", "status": "VACCINATED" }
+{ "id": "160419001", "user_name":"Vincent", "birth_of_date": "2000/01/01", "phone": "085363010101", "photo_url": "https://randomuser.me/api/portraits/men/1.jpg", "status": "NOT VACCINATED" }
